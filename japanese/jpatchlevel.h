f12db074a2ecb66c9b59b2259b6ae900988ef367
SHIRAKATA Kentaro <argrath@ub32.org>
2022-10-25 17:35:48 +0900

    add japanese/

---
+++ japanese/jpatchlevel.h
@@ -0,0 +1,9 @@
+/* JNetHack Copyright */
+/* (c) Issei Numata 1994-2000                                      */
+/* For 3.4-, Copyright (c) SHIRAKATA Kentaro, 2002-2022            */
+/* JNetHack may be freely redistributed.  See license for details. */
+
+#define JVERSION_MAJOR	0
+#define JVERSION_MINOR	6
+#define JPATCHLEVEL	0
+#define JEDITLEVEL	0
