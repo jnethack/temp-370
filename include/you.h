50f16af69367357eac7da7524f531cb7393ff834
SHIRAKATA Kentaro <argrath@ub32.org>
2023-02-07 18:34:21 +0900

    patched you.h

--- include/you.h
+++ include/you.h
@@ -297,6 +297,9 @@ struct Gender {
     const char *him;      /* him/her/it */
     const char *his;      /* his/her/its */
     const char *filecode; /* file code */
+#if 1 /*JP*/
+    const char *opt;      /* オプション指定文字列 */
+#endif
     short allow;          /* equivalent ROLE_ mask */
 };
 #define ROLE_GENDERS 2    /* number of permitted player genders
@@ -328,6 +331,9 @@ struct Align {
     const char *noun;     /* law/balance/chaos */
     const char *adj;      /* lawful/neutral/chaotic */
     const char *filecode; /* file code */
+#if 1 /*JP*/
+    const char *opt;      /* オプション指定文字列 */
+#endif
     short allow;          /* equivalent ROLE_ mask */
     aligntyp value;       /* equivalent A_ value */
 };
